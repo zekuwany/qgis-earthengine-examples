8e5205ce889ca6a4bc5099d03a746f230bb6eca3
Zekwan ShekYusuf Ahmed <zekuwany@gmail.com>
2025-09-07 23:50:50 +0300

    Update map_function.py

--- ImageCollection/map_function.py
+++ ImageCollection/map_function.py
@@ -12,8 +12,8 @@ def conditional(image):
 
 # Load a Landsat 8 collection for a single path-row.
 collection = ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA') \
-  .filter(ee.Filter.eq('WRS_PATH', 44)) \
-  .filter(ee.Filter.eq('WRS_ROW', 34))
+  .filter(ee.Filter.eq('WRS_PATH', 166)) \
+  .filter(ee.Filter.eq('WRS_ROW', 54))
 
 
 
@@ -23,8 +23,8 @@ print(collection.map(addTime).getInfo())
 
 # Load a Landsat 8 collection for a single path-row.
 collection = ee.ImageCollection('LANDSAT/LC8_L1T_TOA') \
-  .filter(ee.Filter.eq('WRS_PATH', 44)) \
-  .filter(ee.Filter.eq('WRS_ROW', 34))
+  .filter(ee.Filter.eq('WRS_PATH', 166)) \
+  .filter(ee.Filter.eq('WRS_ROW', 54))
 
 # This function uses a conditional statement to return the image if
 # the solar elevation > 40 degrees.  Otherwise it returns a zero image.
