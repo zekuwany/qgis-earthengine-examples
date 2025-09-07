32db29b9cef28755a828409d8418d259f182c1e0
Zekwan ShekYusuf Ahmed <zekuwany@gmail.com>
2025-09-07 23:51:18 +0300

    Update select_image_by_index.py

--- ImageCollection/select_image_by_index.py
+++ ImageCollection/select_image_by_index.py
@@ -4,8 +4,8 @@ import ee
 from ee_plugin import Map
 
 collection = ee.ImageCollection('LANDSAT/LC08/C01/T1_TOA')\
-    .filter(ee.Filter.eq('WRS_PATH', 44))\
-    .filter(ee.Filter.eq('WRS_ROW', 34))\
+    .filter(ee.Filter.eq('WRS_PATH', 166))\
+    .filter(ee.Filter.eq('WRS_ROW', 54))\
     .filterDate('2014-01-01', '2015-01-01')
 
 image = ee.Image(collection.toList(collection.size()).get(0))  # select by index from 0 to size-1
